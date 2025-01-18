d1455908d76328d602a978b138612437fa759055
Maksim Buga <maksim.booga@gmail.com>
2025-01-18 14:35:15 +0400

    feat: add read-file task solution

---
+++ 01-read-file/index.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+const filePath = path.join(__dirname, 'text.txt');
+const readStream = fs.createReadStream(filePath, 'utf8');
+
+readStream.pipe(process.stdout);
+
+readStream.on('error', (err) => {
+  console.error('error lmao', err.message);
+});
